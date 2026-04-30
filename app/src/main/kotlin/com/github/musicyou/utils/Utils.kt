5c8fff9b5bb0a81bca2028b584ebaf25ec6c7c03
Daniel <daniel-sevillano@hotmail.com>
2026-05-01 00:48:08 +0200

    Fixing thumbnail size (#86)

--- app/src/main/kotlin/com/github/musicyou/utils/Utils.kt
+++ app/src/main/kotlin/com/github/musicyou/utils/Utils.kt
@@ -80,8 +80,8 @@ val Song.asMediaItem: MediaItem
 
 fun String?.thumbnail(size: Int): String? {
     return when {
-        this?.startsWith("https://lh3.googleusercontent.com") == true -> "$this-w$size-h$size"
-        this?.startsWith("https://yt3.ggpht.com") == true -> "$this-w$size-h$size-s$size"
+        this?.startsWith("https://lh3") == true -> "$this-w$size-h$size"
+        this?.startsWith("https://yt3") == true -> "$this-w$size-h$size-s$size"
         else -> this
     }
 }
